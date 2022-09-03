ecba58062d83401cb58e69579429a817c6bbc17b
Alexander Joel <alexanderj146@gmail.com>
2022-09-02 22:34:03 -0300

    creando Display Js

--- index.js
+++ index.js
@@ -8,12 +8,3 @@ const display = new Display(displayValorSuperior, displayValorInferior);
 botonesNumeros.forEach(boton => {
   boton.addEventListener('click', ()=> display.agregarNumero(boton.innerHTML));
 });
-
-
-/* PRUEBA DE CONSOLA
-const calculadora = new Calculadora();
-
-console.log(calculadora.sumar(2,3))
-console.log(calculadora.multiplicar(8,5))
-console.log(calculadora.dividir(6,3))
-console.log(calculadora.restar(5,3))  */
