c0bc5d7cee62e939ff15c1aafba5b02954d3e558
Alexander Joel <alexanderj146@gmail.com>
2022-09-02 22:31:05 -0300

    creando operaciones Js

--- Display.js
+++ Display.js
@@ -1,5 +1,20 @@
 /* va ser la encargada de controlar la calculadora
 interactua con los botones y muestra en pantalla */
 class Display {
-  constructor(display)
+  constructor(displayValorSuperior, displayValorInferior){
+    this.displayValorInferior = displayValorSuperior;
+    this.displayValorSuperior = displayValorInferior;
+    this.calculador = new calculadora();
+    this.ValorInferior = '';
+    this.ValorSuperior = '';
+  }
+  agregarNumero(numero) {
+    this.ValorInferior = numero;
+    this.imprimirValores();
+  }
+
+  imprimirValores() {
+    this.displayValorInferior.textContent = this.ValorInferior;
+    this.displayValorSuperior.textContent = this.ValorSuperior;
+  }
 }
--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const displayValorInferior = document.getElementById('valorInferior');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
+const display = new Display(displayValorSuperior, displayValorInferior);
+
+botonesNumeros.forEach(boton => {
+  boton.addEventListener('click', ()=> display.agregarNumero(boton.innerHTML));
+});
+
+
+/* PRUEBA DE CONSOLA
 const calculadora = new Calculadora();
 
 console.log(calculadora.sumar(2,3))
 console.log(calculadora.multiplicar(8,5))
 console.log(calculadora.dividir(6,3))
-console.log(calculadora.restar(5,3))
+console.log(calculadora.restar(5,3))  */
